15b0a33618cd6e9e61208ec5cc4fcb6c14f5ae23
Alejandro Hernandez <166560613+HelperDays@users.noreply.github.com>
2025-10-29 22:23:29 -0300

    fix

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './routes/home/home.page';
 import { DashboardPage } from './routes/dashboard/dashboard.page';
-import { LoginPage } from './routes/login/login.page';
+import { LoginPage } from './routes/login/login/login.page';
 import { RegisterPage } from './routes/register/register/register.page';
 import { DeleteAccountPage } from './routes/profile/delete-account/delete-account.page';
 import { EditProfilePage } from './routes/profile/edit-profile/edit-profile.page';
@@ -15,18 +15,18 @@ export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePage, title: 'home' },
   { path: 'dashboard', component: DashboardPage, title: 'dashboard' },
-  { path: 'login', component: LoginPage, title: 'login' },
-  { path: 'register', component: RegisterPage, title: 'register' },
-  { path: 'profile', component: ProfilePage, title: 'perfil' },
-  { path: 'profile/edit', component: EditProfilePage, title: 'editar perfil' },
-  { path: 'perfil/delete', component: DeleteAccountPage, title: 'eliminar cuenta' },
+  { path: 'login', component: LoginPage, title: 'login' }, //ruta no funciona
+  { path: 'register', component: RegisterPage, title: 'register' }, //funciona la ruta
+  { path: 'profile', component: ProfilePage, title: 'perfil' }, //funciona la ruta
+  { path: 'profile/edit', component: EditProfilePage, title: 'editar perfil' }, //funciona la ruta
+  { path: 'perfil/delete', component: DeleteAccountPage, title: 'eliminar cuenta' }, //ruta no funciona
   { path: 'services/my-services', component: MyServicePage, title: 'Mis servicios' },
   {
     path: 'services/my-services/edit/:serviceId',
     component: EditServicePage,
     title: 'Editar servicio',
   },
-  { path: 'services/my-services/create', component: CreateServicePage, title: 'Crear servicio' },
+  { path: 'services/create', component: CreateServicePage, title: 'Crear servicio' },
   {
     path: 'services/my-services/detail/:serviceId',
     component: DetailServicePage,
