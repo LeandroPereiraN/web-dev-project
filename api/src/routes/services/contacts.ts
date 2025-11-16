1a4e45410de477dfecf6de6263c3a3110b19766c
Leandro Pereira <leandro.pereiran23@gmail.com>
2025-11-16 18:49:44 -0300

    Fix

--- api/src/routes/services/contacts.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyInstance } from "fastify";
-
-export default async function legacyServiceContactsRoutes(
-  _fastify: FastifyInstance
-) {
-  // Route moved into services/index.ts to avoid duplicated prefixes with autoload.
-}
