ec2b7750ea69895f9eb68efdcaeffbac553f306f
Leandro Pereira <leandro.pereiran23@gmail.com>
2025-11-19 21:07:11 -0300

    Add android apk

---
+++ front/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.superservice.app',
+  appName: 'super-service',
+  webDir: 'dist/superService/browser/',
+};
+
+export default config;
